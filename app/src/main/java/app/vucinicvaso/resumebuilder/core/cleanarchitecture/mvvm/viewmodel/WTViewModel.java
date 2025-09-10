84dbecaa5ea172dc7d6da91ee3c51d6d7a7b1767
vaso.vucinic <vasoct@gmail.com>
2025-09-10 15:44:11 +0200

    updated ViewModel

--- app/src/main/java/app/vucinicvaso/resumebuilder/core/cleanarchitecture/mvvm/viewmodel/WTViewModel.java
+++ app/src/main/java/app/vucinicvaso/resumebuilder/core/cleanarchitecture/mvvm/viewmodel/WTViewModel.java
@@ -1,3 +1,12 @@
 package app.vucinicvaso.resumebuilder.core.cleanarchitecture.mvvm.viewmodel;
+import android.content.Context;
+import app.vucinicvaso.resumebuilder.core.router.impl.WTRouterImpl;
 
-public abstract class WTViewModel {}
+public abstract class WTViewModel {
+
+    public void navigateTo(Context context, String route) throws ClassNotFoundException {
+        var router = WTRouterImpl.getInstance();
+        router.navigateTo(context, route);
+    }
+
+}
--- app/src/main/java/app/vucinicvaso/resumebuilder/presentation/viewmodel/main/MainViewModel.java
+++ app/src/main/java/app/vucinicvaso/resumebuilder/presentation/viewmodel/main/MainViewModel.java
@@ -2,8 +2,6 @@ package app.vucinicvaso.resumebuilder.presentation.viewmodel.main;
 
 import android.content.Context;
 import app.vucinicvaso.resumebuilder.core.cleanarchitecture.mvvm.viewmodel.WTViewModel;
-import app.vucinicvaso.resumebuilder.core.router.WTRouter;
-import app.vucinicvaso.resumebuilder.core.router.impl.WTRouterImpl;
 import app.vucinicvaso.resumebuilder.domain.entity.account.Account;
 import app.vucinicvaso.resumebuilder.domain.usecase.account.AccountExistsUseCase;
 import app.vucinicvaso.resumebuilder.domain.usecase.account.AccountCreateUseCase;
@@ -14,24 +12,26 @@ public class MainViewModel extends WTViewModel {
     public MainViewModel() {}
 
     public void init(Context context) {
-        new AccountExistsUseCase().call(context, response -> {
-            if(response) { redirect(context); }
-            if(!response) { createAccount(context); }
-        });
+        new AccountExistsUseCase().call(
+            context,
+            response -> {
+                if(response) {  navigateTo(context, "dashboard"); }
+                if(!response) { createAccount(context); }
+            }
+        );
     }
 
     public void createAccount(Context context) {
         Account account = new Account(0, "", "", "", "");
 
-        new AccountCreateUseCase().call(context, new AccountCreateUseCaseParams(account), response -> {
-            if(response) { redirect(context); }
-            if(!response) { System.out.println("failed to create new account"); }
-        });
-    }
-
-    public void redirect(Context context) throws ClassNotFoundException {
-        WTRouter router = WTRouterImpl.getInstance();
-        router.navigateTo(context, "dashboard");
+        new AccountCreateUseCase().call(
+            context,
+            new AccountCreateUseCaseParams(account),
+            response -> {
+                if(response) { navigateTo(context, "dashboard"); }
+                if(!response) { System.out.println("failed to create new account"); }
+            }
+        );
     }
 
 }
